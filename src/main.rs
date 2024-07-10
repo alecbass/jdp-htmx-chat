27e86477e333ab6f4c0f1badcd772137e6e3066c
alecbass <alec.bassingthwaighte@gmail.com>
2024-07-10 20:29:35 +1000

    Add messages via HTMX

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,56 @@
+use rocket::form::Form;
+use rocket::fs::FileServer;
 use rocket_dyn_templates::{context, Template};
 
 #[macro_use]
 extern crate rocket;
 
+///
+/// GET request to load the index page
+///
 #[get("/")]
 fn index() -> Template {
+    let messages = vec!["index", "messages"];
+
     Template::render(
         "index",
         context! {
-            message: "Page"
+            messages: messages
         },
     )
 }
 
-#[post("/message")]
-fn create_message() -> Template {
+#[derive(FromForm)]
+struct CreateMessageRequest {
+    message: String,
+}
+
+///
+/// GET request to load all messages
+///
+#[get("/message")]
+fn get_messages() -> Template {
+    let messages = vec!["hellooo", "epic"];
+
     Template::render(
-        "index",
+        "messages",
         context! {
-            message: "Hello, world!"
+            messages: messages
+        },
+    )
+}
+
+///
+/// POST request to create a new message, and return the current list of messages as HTML
+///
+#[post("/create-message", data = "<message>")]
+fn create_message(message: Form<CreateMessageRequest>) -> Template {
+    let messages = vec!["hellooo", "epic", &message.message];
+
+    Template::render(
+        "messages",
+        context! {
+            messages: messages
         },
     )
 }
@@ -26,6 +58,7 @@ fn create_message() -> Template {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, create_message])
+        .mount("/", routes![index, get_messages, create_message])
+        .mount("/static", FileServer::from("static"))
         .attach(Template::fairing())
 }
