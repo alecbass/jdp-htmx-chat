73f63a2d996300a0e2ff2ba841dc8004f25619f5
alecbass <alec.bassingthwaighte@gmail.com>
2024-07-17 21:50:36 +1000

    Remove unhealthy websockets

--- src/websocket.rs
+++ src/websocket.rs
@@ -17,14 +17,23 @@ impl WebSocketHandler {
     }
 
     pub fn broadcast(&mut self, message: &str) -> Result<(), Error> {
-        for websocket in self.websockets.iter_mut() {
-            // if let Err(e) = websocket.read() {
-            //     eprintln!("Error reading from websocket: {}", e);
-            // }
+        let mut unhealthy_indexes = Vec::<usize>::new();
 
+        for (index, websocket) in self.websockets.iter_mut().enumerate() {
             if let Err(e) = websocket.send(Message::Text(message.to_string())) {
+                unhealthy_indexes.push(index);
                 eprintln!("Error sending message: {}", e);
             }
+        }
+
+        for index in unhealthy_indexes.iter().rev() {
+            println!(
+                "Removing websocket at index {} due to it being unhealthy",
+                index
+            );
+
+            // Remove all websockets that failed to be written to
+            self.websockets.remove(*index);
         }
 
         Ok(())
